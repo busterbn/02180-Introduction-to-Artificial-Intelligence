8ca5a97d93a20499426f755cae8a6a1a90733043
Buster Nielsen <bn@mac.local>
2025-03-23 13:55:43 +0100

    fixed a bug

--- src/min_max.py
+++ src/min_max.py
@@ -34,6 +34,7 @@ def minimax(game, depth):
                 if score_temp > best_score:
                     best_score = score_temp
                     best_move = pit
+        return best_score, best_move
     
     elif game.current_player == 1:
         # Here we do the inverse for the minimizing player
@@ -51,13 +52,69 @@ def minimax(game, depth):
                 if score_temp < best_score:
                     best_score = score_temp
                     best_move = pit
-    
-    return best_score, best_move
+        return best_score, best_move
+
+def minimax_alpha_beta(game, depth, alpha=0, beta=0):
+    best_move = -1
+    if depth == 0 or game.is_game_over():
+        final_score = game.board[13] - game.board[6]
+        return final_score, best_move
+
+    if game.current_player == 2:
+        best_score = float("-inf")
+        for pit in range(7, 13):
+            if game.is_valid_move(pit):
+                game_copy = copy.deepcopy(game)
+                game_copy.make_move(pit)
+                if game_copy.current_player == 2:
+                    score_temp, _ = minimax_alpha_beta(game_copy, depth, alpha, beta)
+                else:
+                    score_temp, _ = minimax_alpha_beta(game_copy, depth - 1, alpha, beta)
+                if score_temp > best_score:
+                    best_score = score_temp
+                    best_move = pit
+                alpha = max(alpha, best_score)
+                if beta <= alpha:
+                    break
+        return best_score, best_move
+
+    elif game.current_player == 1:
+        best_score = float("inf")
+        for pit in range(0, 6):
+            if game.is_valid_move(pit):
+                game_copy = copy.deepcopy(game)
+                game_copy.make_move(pit)
+                if game_copy.current_player == 1:
+                    score_temp, _ = minimax_alpha_beta(game_copy, depth, alpha, beta)
+                else:
+                    score_temp, _ = minimax_alpha_beta(game_copy, depth - 1, alpha, beta)
+                if score_temp < best_score:
+                    best_score = score_temp
+                    best_move = pit
+                beta = min(beta, best_score)
+                if beta <= alpha:
+                    break
+        return best_score, best_move
                 
 
 def play_game(depth):
     game = Kalah()
     last_to_move = '0'
+        
+    while True:
+        print("Who should start?")
+        print("1: You")
+        print("2: The AI")
+        val = int(input(""))
+        if val == 1:
+            break
+        elif val == 2:
+            game.switch_start_position()
+            break
+        else:
+            print("Invalid choise !!")
+
+
     game.display_board()
     while not game.is_game_over():
         if game.current_player == 1:
@@ -67,15 +124,7 @@ def play_game(depth):
                         print("It's your turn again !!")
                     else:
                         print("It's your turn")
-                    if game.is_first_move:
-                        text = input("Please choose a pit (1-6) or write 'skip' to let the AI start: ")
-                        if text == "skip":
-                            game.switch_start_position()
-                            print(f"player={game.current_player}")
-                            break
-                        pip = int(text) - 1
-                    else:
-                        pit = int(input("Please choose a pit (1-6): ")) - 1
+                    pit = int(input("Please choose a pit (1-6): ")) - 1
                     if game.make_move(pit):
                         print(f"You moved pit {pit+1}")
                         time.sleep(1)
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -3,6 +3,57 @@ from kalah import Kalah
 import math
 import time
 
+def minimax(game, depth):
+    best_move = -1
+    # When the game is over or max depth is reached the board is evaluated
+    if depth == 0 or game.is_game_over():
+        final_score = game.board[13] - game.board[6]
+        return final_score, best_move
+    
+    # Finding the AI's best move
+    if game.current_player == 2:
+        best_score = float("-inf")
+
+        # Loop over all moves
+        for pit in range(7, 13):
+
+            # Discard impossible moves
+            if game.is_valid_move(pit):
+
+                # Simulate the move
+                game_copy = copy.deepcopy(game)
+                game_copy.make_move(pit)
+                
+                # The depth is not decremented when a player gets an extra turn
+                if game_copy.current_player == 2:
+                    score_temp, _ = minimax(game_copy, depth)
+                else:
+                    score_temp, _ = minimax(game_copy, depth-1)
+                
+                # Check if the move is the best one yet.
+                if score_temp > best_score:
+                    best_score = score_temp
+                    best_move = pit
+        return best_score, best_move
+    
+    elif game.current_player == 1:
+        # Here we do the inverse for the minimizing player
+        best_score = float("inf")
+        for pit in range(0, 6):
+            if game.is_valid_move(pit):
+                game_copy = copy.deepcopy(game)
+                game_copy.make_move(pit)
+
+                if game_copy.current_player == 1:
+                    score_temp, _ = minimax(game_copy, depth)
+                else:
+                    score_temp, _ = minimax(game_copy, depth-1)
+
+                if score_temp < best_score:
+                    best_score = score_temp
+                    best_move = pit
+        return best_score, best_move
+
 def minimax_alpha_beta(game, depth, alpha=0, beta=0):
     best_move = -1
     if depth == 0 or game.is_game_over():
@@ -49,21 +100,6 @@ def minimax_alpha_beta(game, depth, alpha=0, beta=0):
 def play_game(depth):
     game = Kalah()
     last_to_move = '0'
-        
-    while True:
-        print("Who should start?")
-        print("1: You")
-        print("2: The AI")
-        val = int(input(""))
-        if val == 1:
-            break
-        elif val == 2:
-            game.switch_start_position()
-            break
-        else:
-            print("Invalid choise !!")
-
-
     game.display_board()
     while not game.is_game_over():
         if game.current_player == 1:
@@ -73,7 +109,15 @@ def play_game(depth):
                         print("It's your turn again !!")
                     else:
                         print("It's your turn")
-                    pit = int(input("Please choose a pit (1-6): ")) - 1
+                    if game.is_first_move:
+                        text = input("Please choose a pit (1-6) or write 'skip' to let the AI start: ")
+                        if text == "skip":
+                            game.switch_start_position()
+                            print(f"player={game.current_player}")
+                            break
+                        pip = int(text) - 1
+                    else:
+                        pit = int(input("Please choose a pit (1-6): ")) - 1
                     if game.make_move(pit):
                         print(f"You moved pit {pit+1}")
                         time.sleep(1)
@@ -90,7 +134,7 @@ def play_game(depth):
             else:
                 print(f"It's MiniMax's turn")
             time.sleep(1)
-            score, move = minimax_alpha_beta(game, depth)
+            score, move = minimax(game, depth)
             print(f"MiniMax moves pit {move-6} with anticipated score {score}")
             game.make_move(move)
             time.sleep(1)
@@ -103,5 +147,5 @@ def play_game(depth):
     print(game.get_winner())
 
 if __name__ == "__main__":
-    play_game(8)
+    play_game(6)
 
